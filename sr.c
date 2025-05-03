c4f6ba2d0f3f5cfa89216e3b100c3c725f5f9ec2
thomasnmd <a1868322@adelaide.edu.au>
2025-05-03 20:58:33 +0930

    fix

--- sr.c
+++ sr.c
@@ -119,8 +119,7 @@ void A_input(struct pkt packet)
   if (!IsCorrupted(packet)) {
     if (TRACE > 0)
       printf("----A: uncorrupted ACK %d is received\n",packet.acknum);
-    total_ACKs_received++;
-    acked[packet.acknum] = true;
+    return;
 
     /*check if wincount bigger than the next*/
     while (windowcount > 0 && acked[windowfirst]) {
